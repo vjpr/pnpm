30d98bd6d1b6ba1f620ee29f78727ed9d44940f8
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-08-22 21:36:59 +0300

    feat: publish a bundled version of the package

--- .scripts/rename_key.js
+++ .scripts/rename_key.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = (opts) => {
+module.exports = opts => {
   const pkgPath = path.resolve(process.cwd(), 'package.json')
   const rawPkgJSON = fs.readFileSync(pkgPath, 'UTF8')
   const pkgJSON = JSON.parse(rawPkgJSON)
@@ -10,12 +10,22 @@ module.exports = (opts) => {
   const newPkgJSON = {}
   const keys = Object.keys(pkgJSON)
   for (let i = 0; i < keys.length; i++) {
+    if (keys[i] === 'scripts') {
+      newPkgJSON.scripts = pkgJSON.scripts
+      if (opts.pkgName === 'pnpm-rocket') {
+        newPkgJSON.scripts.preinstall = 'node .scripts/try_rename cached_node_modules node_modules'
+        continue
+      }
+      delete newPkgJSON.scripts.preinstall
+      continue
+    }
     if (keys[i] === opts.currentKeyName) {
       newPkgJSON[opts.newKeyName] = pkgJSON[opts.currentKeyName]
       continue
     }
     newPkgJSON[keys[i]] = pkgJSON[keys[i]]
   }
+  newPkgJSON.name = opts.pkgName
 
   fs.writeFileSync(pkgPath, JSON.stringify(newPkgJSON, null, 2), 'UTF8')
 }
